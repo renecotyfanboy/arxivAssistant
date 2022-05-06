d79161babd866649019d5f941634ec155803c2dd
sdupourque <sdupourque@irap.omp.eu>
2022-05-07 01:32:48 +0200

    made article Renderable through rich

--- arxivAssistant/article.py
+++ arxivAssistant/article.py
@@ -1,8 +1,10 @@
 import re
 from rich.console import Console
+from rich.text import Text
+from rich.panel import Panel
 from typing import NamedTuple
 from html import unescape
-
+from rich.console import Group
 
 def extract_text(html):
     res = re.search(r'>(.*)<', html)
@@ -30,10 +32,30 @@ class Article(NamedTuple):
             entry['link']
         )
 
+    def __rich__(self):
+
+        if len(self.authors) > 10:
+            author_list = self.authors[:10] + ["et al."]
+        else:
+            author_list = self.authors
+
+        # make a string with all the authors separated by commas
+        authors = ", ".join(author_list)
+        # decode numerical unicode characters in the authors string
+        authors = unescape(authors)
+
+        panel_group = Group(
+            Panel(self.title, style="bold red"),
+            Panel(f"{authors}", style="italic green"),
+            Panel(Text(self.abstract, justify="full"))
+        )
+
+        return panel_group#text
+
     # make a method to print the article in a rich way as done in today.py
     def to_console(self, console: Console):
 
-        console.rule(self.title.split('(')[0], style="bold red")
+        console.rule(self.title, style="bold red")
         # this should be done in the feed class
         # if there are more than 10 authors, keep only the first 10 and replace the rest with "et al."
         if len(self.authors) > 10:
