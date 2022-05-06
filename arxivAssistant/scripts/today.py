38b460ac5a2bf3897b655b5d10c1767702f40301
sdupourque <sdupourque@irap.omp.eu>
2022-05-07 01:39:17 +0200

    arxiv-today now launches the main application

--- arxivAssistant/scripts/today.py
+++ arxivAssistant/scripts/today.py
@@ -1,13 +1,6 @@
-from arxivAssistant.feed import ArxivFeed
-from rich.console import Console
+from arxivAssistant.app import ArxivApp
 
 
 def main() -> None:
 
-    console = Console()
-
-    with console.status("Fetching today's arxiv..."):
-        today = ArxivFeed()
-
-    for article in today.articles:
-        article.to_console(console)
+    ArxivApp.run(title="Arxiv Assistant")
