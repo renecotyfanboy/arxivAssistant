edb3cc0c4c0c2dfe383428ed7d74bc3b8a00d234
sdupourque <sdupourque@irap.omp.eu>
2022-05-07 18:42:34 +0200

    enforce python 3.10

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ __version__ = 0.3
 setup(
     name='arxivAssistant',
     version=str(__version__),
+    python_requires='>=3.10',
     description='Read daily arxiv feed with console',
     author='Simon Dupourqué',
     author_email='sdupourque@irap.omp.eu',
