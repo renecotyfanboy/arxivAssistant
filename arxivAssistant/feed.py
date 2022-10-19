f963078d460ab80ef3a4d1548eaf7511440d4293
sdupourque <sdupourque@irap.omp.eu>
2022-10-19 10:59:26 +0200

    using 'https'

--- arxivAssistant/feed.py
+++ arxivAssistant/feed.py
@@ -4,7 +4,7 @@ from .article import Article
 
 class ArxivFeed:
 
-    def __init__(self, feed_url='http://arxiv.org/rss/astro-ph'):
+    def __init__(self, feed_url='https://arxiv.org/rss/astro-ph'):
         self.feed_url = feed_url
         self.feed = feedparser.parse(self.feed_url)
 
